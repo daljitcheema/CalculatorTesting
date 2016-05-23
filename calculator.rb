0734c4e3891870d2af81860dd3c61080ea20ed5c
daljitcheema <daljit.cheema@me.com>
2016-05-23 12:15:49 +0100

    check accumulator is floating point

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,16 @@
 class Calculator
   attr_accessor :accumulator 
     
+    def add(val)
+      self.accumulator += val
+    end
+    def subtract(val)
+      self.accumulator -= val
+    end
+    def multiply(val)
+      self.accumulator *= val      
+    end
+    def divide(val)
+      self.accumulator /= val
+    end
   end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -13,20 +13,31 @@ class CalculatorSpec <MiniTest::Test
     my_calculator = Calculator.new
     my_calculator.accumulator = 5
     assert_equal(my_calculator.add(7), 12)
+    assert_equal(0, my_calculator.add(-12))
   end
   def test_multiply
       my_calculator = Calculator.new
       my_calculator.accumulator = 5
       assert_equal(my_calculator.multiply(7), 35)
+       assert_equal(70, my_calculator.multiply(2))
   end
   def test_divide
       my_calculator = Calculator.new
       my_calculator.accumulator =100
       assert_equal(my_calculator.divide(50), 2)
+      assert_equal(1, my_calculator.divide(2))
+      assert_equal()
   end
+  def test_accumulator_is_floating_point
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_instance_of(Float, my_calculator.accumulator)
+  end
+
   def test_subtract
       my_calculator = Calculator.new
       my_calculator.accumulator =50
       assert_equal(my_calculator.subtract(35), 15)
+      assert_equal(10, my_calculator.subtract(5))    
   end
 end
