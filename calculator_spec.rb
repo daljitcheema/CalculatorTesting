10dc686c4d153f83da3641e41df29389ee82190e
daljitcheema <daljit.cheema@me.com>
2016-05-23 11:56:58 +0100

    add multiple and subtract tests

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -12,7 +12,21 @@ class CalculatorSpec <MiniTest::Test
   def test_add
     my_calculator = Calculator.new
     my_calculator.accumulator = 5
-
     assert_equal(my_calculator.add(7), 12)
+  end
+  def test_multiply
+      my_calculator = Calculator.new
+      my_calculator.accumulator = 5
+      assert_equal(my_calculator.multiply(7), 35)
+  end
+  def test_divide
+      my_calculator = Calculator.new
+      my_calculator.accumulator =100
+      assert_equal(my_calculator.divide(50), 2)
+  end
+  def test_subtract
+      my_calculator = Calculator.new
+      my_calculator.accumulator =50
+      assert_equal(my_calculator.subtract(35), 15)
   end
 end
